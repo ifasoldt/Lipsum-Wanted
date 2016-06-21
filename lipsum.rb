447e696ea1c7dbb42313f2badff95b201a14433a
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-06-20 21:46:04 -0400

    fixed error message when lipsum called with no arguments.

--- lipsum.rb
+++ lipsum.rb
@@ -3,7 +3,11 @@
 require 'erb'
 require 'shellwords'
 
-type = ARGV[0]
+type_natural = ARGV[0]
+if type_natural == nil
+  abort ("I'm sorry, you need to make a choice of lipsum between 'Hipster', 'NotLorem', and 'Fillerama'")
+end
+type = type_natural.downcase
 paragraph_num = ARGV[1].to_i
 if paragraph_num == 0
   paragraph_num = 1
@@ -12,19 +16,19 @@ generated = []
 i = 0
 
 
-if type.downcase == "hipster"
+if type == "hipster"
   while paragraph_num > i
     generated.push("Pabst chartreuse selfies, YOLO kogi normcore franzen pop-up messenger bag. Mustache street art neutra flexitarian, DIY cred direct trade butcher flannel pinterest whatever food truck roof party. Try-hard hella affogato umami occupy, schlitz tattooed PBR&B austin kombucha. Tofu swag seitan bushwick dreamcatcher gentrify, four loko aesthetic wayfarers biodiesel. Viral echo park freegan cliche. Cred mlkshk selvage, williamsburg you probably haven't heard of them banh mi swag kale chips VHS. YOLO intelligentsia cardigan selvage. ")
     i += 1
   end
 system("echo #{Shellwords.escape(generated[0])} | pbcopy")
-elsif type.downcase == "notlorem"
+elsif type == "notlorem"
   while paragraph_num > i
     generated.push("We are County Council Trading Standards Approved as part of the Buy With Confidence scheme and all of our vehicles are HPI checked. So you know that your car is genuinely available for sale, doesn’t have any outstanding finance on it and that we are a reputable dealer. We also verify the mileage on all of the cars that we are selling and ensure that they are all sold with long MOTs and road tax. ")
     i += 1
   end
 system("echo #{Shellwords.escape(generated[0])} | pbcopy")
-elsif type.downcase == "fillerama"
+elsif type == "fillerama"
   while paragraph_num > i
     generated.push ("I love this planet! I've got wealth, fame, and access to the depths of sleaze that those things bring. And then the battle's not so bad? Now what? Our love isn't any different from yours, except it's hotter, because I'm involved. ")
     i += 1
